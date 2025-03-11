56f8a52e27f0aed58d5d93d49b5b6ba7cc69cc52
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-11 22:01:59 +0600

    added array task 1

---
+++ DSA-MIDTERM-PRACTICE-TOPICS/array-task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[4] = {10, 20, 30, 40};
+    cout << array[0] << " ";
+    cout << array[1] << " ";
+    cout << array[2] << " ";
+    cout << array[3] << " " << endl;
+    return 0;
+}
